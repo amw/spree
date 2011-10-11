8ba9b6b23ba45e194064e3ad2a396afc28b3a6e2
Adam Wróbel <adam@fluxinc.ca>
2011-10-11 09:21:50 +0200

    Show only selected payment method in admin.

--- core/public/javascripts/admin/payments/new.js
+++ core/public/javascripts/admin/payments/new.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   
   $("#card_new").radioControlsVisibilityOfElement('#card_form');
+
+  $('input[name="payment[payment_method_id]"]').change(function() {
+    console.log('im changed!');
+    var form = $(this).parents('form');
+    form.children('label').css('display','block');
+    form.children('fieldset').hide();
+    $(this).parent('label').next('fieldset').show();
+  }).filter(':checked').trigger('change');
   
   $('select.jump_menu').change(function(){
     window.location = this.options[this.selectedIndex].value;
