da689393c7884dada3a07f825917a3fe9b323630
Adam Wróbel <adam@fluxinc.ca>
2012-05-12 19:51:14 +0200

    Don't clear form data when forcing guest checkout.

--- core/public/javascripts/admin/checkouts/edit.js
+++ core/public/javascripts/admin/checkouts/edit.js
@@ -95,29 +95,6 @@ jQuery(document).ready(function(){
 
   $('#guest_checkout_true').change(function() {
     $('#customer_search').val("");
-    $('#user_id').val("");
-    $('#checkout_email').val("");
-    $('#guest_checkout_false').attr("disabled", "true");
-
-    $('#order_bill_address_attributes_firstname').val("");
-    $('#order_bill_address_attributes_lastname').val("");
-    $('#order_bill_address_attributes_address1').val("");
-    $('#order_bill_address_attributes_address2').val("");
-    $('#order_bill_address_attributes_city').val("");
-    $('#order_bill_address_attributes_zipcode').val("");
-    $('#order_bill_address_attributes_state_id').val("");
-    $('#order_bill_address_attributes_country_id').val("");
-    $('#order_bill_address_attributes_phone').val("");
-
-    $('#order_ship_address_attributes_firstname').val("");
-    $('#order_ship_address_attributes_lastname').val("");
-    $('#order_ship_address_attributes_address1').val("");
-    $('#order_ship_address_attributes_address2').val("");
-    $('#order_ship_address_attributes_city').val("");
-    $('#order_ship_address_attributes_zipcode').val("");
-    $('#order_ship_address_attributes_state_id').val("");
-    $('#order_ship_address_attributes_country_id').val("");
-    $('#order_ship_address_attributes_phone').val("");
   });
 
   var show_billing = function(show) {
